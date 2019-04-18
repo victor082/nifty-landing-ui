e9d3f59997cea22093d13d09a30c08f1fbb25990
victorN <victornguyen082@gmail.com>
2019-04-18 16:19:29 -0400

    fav icon

--- Javascript/home-page.js
+++ Javascript/home-page.js
@@ -1,15 +1,3 @@
-let button = document.querySelector('.welcomeBox .signUpBtn')
-button.addEventListener('mouseenter', (event) => {
-    button.style.background = 'white'
-    button.style.color = '#3384c6'
-    button.innerHTML = 'SIGN UP'
-  })
-  button.addEventListener('mouseleave', (event) => {
-    button.style.background = 'linear-gradient(to right, #3384c6, #25b7c4)'
-    button.style.background = 'gradient(left, #3384c6, #25b7c4)'
-    button.style.color = 'white'
-    button.innerHTML = 'GET STARTED'
-  })
   var accItem = document.getElementsByClassName('accordionItem');
   var accHD = document.getElementsByClassName('accordionItemHeading');
   for (i = 0; i < accHD.length; i++) {
